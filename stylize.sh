dc19ecfdd07c0534495630b450ba2b34a0e62df4
Philip Ottesen <pjo256@nyu.edu>
2019-03-13 18:43:52 -0400

    fix: stylize.sh args

--- stylize.sh
+++ stylize.sh
@@ -1,8 +1,8 @@
-local VIDEO_URL = $0
-local STYLE = $1
-local OUTPUT_PATH = $2
+VIDEO_URL=$0
+STYLE=$1
+OUTPUT_PATH=$2
 
-INPUT_PATH = "./input/$VIDEO_URL-raw.mp4"
+INPUT_PATH="./input/$VIDEO_URL-raw.mp4"
 
 youtube-dl -o $INPUT_PATH $VIDEO_URL
 python transform_video.py --in-path $INPUT_PATH \
